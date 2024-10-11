2e874155d98885e4cc173a4ea79fce47dc543d9a
yousef makkawy <yousef.elmakkawy@gmail.com>
2024-10-11 17:29:37 +0300

    add repositories

---
+++ CQRS/Repositories/IItemRepository.cs
@@ -0,0 +1,13 @@
+﻿using CQRS.Data.Models;
+
+namespace CQRS.Repositories
+{
+    public interface IItemRepository
+    {
+        public List<Item> GetAllItems();
+        public Item GetById(int id);
+        public int AddItem(Item item);
+        public int UpdateItem(Item item);
+        public int DeleteItem(int id);
+    }
+}
---
+++ CQRS/Repositories/ItemRepository.cs
@@ -0,0 +1,32 @@
+﻿using CQRS.Data.Models;
+
+namespace CQRS.Repositories
+{
+    public class ItemRepository : IItemRepository
+    {
+        public int AddItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int DeleteItem(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Item> GetAllItems()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Item GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int UpdateItem(Item item)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
